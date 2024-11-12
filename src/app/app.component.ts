10a2a105741839bdf3694af9f7a087bc1520db9c
MichaelSani05 <msmichaelsani@gmail.com>
2024-11-12 14:59:00 +0100

    V-1.0.6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
 }
